fbceb7b855c7b46f775da00d03219f817afd0cbd
pakiaseelan <42385045+pakiaseelan@users.noreply.github.com>
2026-09-04 15:50:13 +0530

    Update angular-inc-and-pause-with-signal-task-strategy-company-coding-test.js

--- angular-inc-and-pause-with-signal-task-strategy-company-coding-test.js
+++ angular-inc-and-pause-with-signal-task-strategy-company-coding-test.js
@@ -1,1 +1,32 @@
+import { Component, signal, provideZonelessChangeDetection } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
 
+@Component({
+  selector : 'app-root',
+  standalone : true, 
+  template : `<h1>Hello world</h1>
+  <button (click)="incs()">
+  {{count()}}
+  </button>
+  `;
+});
+
+class appComponent {
+  count = signal(0);
+  flag : true;
+  interval : any;
+  
+  incs(){
+    this.flag = (this.flag == true) ? false : true;
+    if(this.flag){
+      this.interval = setInterval(()=>{
+        this.count.set(this.count() + 1);
+      }, 500);
+    }else{
+      clearInterval(this.interval);
+    }
+  }
+}
+bootstrapApplication(AppComponent, {
+  providers : [provideZonelessChangeDection]
+})
